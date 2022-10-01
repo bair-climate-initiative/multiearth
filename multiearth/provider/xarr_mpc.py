6d1ee6288d08262720366fb60879a5a238dd4242
Sean <sean.metzger@ucsf.edu>
2022-09-30 17:34:10 -0700

    passing

--- multiearth/provider/xarr_mpc.py
+++ multiearth/provider/xarr_mpc.py
@@ -1,48 +1,36 @@
 """Microsoft Planetary Computer (MPC) provider."""
-
-from typing import Callable
-
-import planetary_computer as pc
-import pystac
-import pystac_client
-from .mpc import MicrosoftPlanetaryComputer
-import planetary_computer
-
-import fsspec
-import xarray as xr
-import geopandas as gpd
-
-from shapely.geometry import Point
-import numpy as np
-
 from os.path import join
 
+import geopandas as gpd
+import numpy as np
+import planetary_computer
+import xarray as xr
+from shapely.geometry import Point, Polygon
+
 from ..util.datetime import datetime_str_to_value
+from .mpc import MicrosoftPlanetaryComputer
+
 
 class XarrMPC(MicrosoftPlanetaryComputer):
-    """Download data and extract xarray assets from MPC"""
-     
-    def get_mask(self, ds, aoi): 
-        """
-        take the dataset and the area of interest and make a mask 1 if the lat/lon
-        is within the area of interest, and np.nan otherwise. 
-        """
+    """Download data and extract xarray assets from MPC."""
+
+    def get_mask(self, ds: xr.DataSet, aoi: Polygon) -> np.array:
+        """Return a mask for resulting values that gets the area of interest."""
         mask = np.empty((len(ds.lat.values), len(ds.lon.values)))
         mask[:] = np.nan
-        for i, lat in enumerate((ds.lat.values)):
-            for j, lon in enumerate((ds.lon.values)):
+        for i, lat in enumerate(ds.lat.values):
+            for j, lon in enumerate(ds.lon.values):
 
                 if aoi.contains(Point(lon, lat)):
                     mask[i, j] = 1
         return mask
 
-    def extract_assets(self, dry_run: bool=False) -> bool:
-        """Download a dataset to assigned output dir"""
-        # TODO: use the inhereted 
+    def extract_assets(self, dry_run: bool = False) -> bool:
+        """Download a dataset to assigned output dir."""
+        # TODO: use the inhereted
         catalog = self._client
-            
-        for collection in self.collections: 
-            dataset=collection.id
+
+        for collection in self.collections:
             assert (
                 collection.datetime is not None
             ), "Collection {dataset_id} datetime is not set"
@@ -52,53 +40,52 @@ class XarrMPC(MicrosoftPlanetaryComputer):
             # Try to get the dataset from MPC
             cat = catalog.get_collection(collection.id)
             asset = planetary_computer.sign(cat.assets["zarr-abfs"])
-            
+
             ds = xr.open_zarr(
                 asset.href,
                 storage_options=asset.extra_fields["xarray:storage_options"],
-                **asset.extra_fields["xarray:open_kwargs"]
+                **asset.extra_fields["xarray:open_kwargs"],
             )
-            
+
             # Select just the relevant features.
-            if not collection.assets == 'all':
-                ds = ds[list(collection.assets)]
-            
-            # Select just the relevant datetime. 
-            if not collection.datetime == '..':
+            if not collection.assets == ["all"]:
+                ds = ds[[collection.assets]]
+
+            # Select just the relevant datetime.
+            if not collection.datetime == "..":
                 start_date, end_date = datetime_str_to_value(collection.datetime)
                 ds = ds.sel(time=slice(start_date, end_date))
-                
-            # Select just the relevant area. 
+
+            # Select just the relevant area.
             aoi = None
             if collection.aoi_file is not None:
-               
+
                 aoi = gpd.read_file(collection.aoi_file).unary_union
-                ds = ds.loc[dict(lon = slice(aoi.bounds[0], aoi.bounds[2]))]
-                # TODO: can we make this also use loc? Wasn't working for me. 
-                latinds = np.intersect1d(np.where(ds.lat >= aoi.bounds[1]), np.where(ds.lat <= aoi.bounds[3]))
-                ds = ds.isel(dict(lat = latinds))
+                ds = ds.loc[dict(lon=slice(aoi.bounds[0], aoi.bounds[2]))]
+                # TODO: can we make this also use loc? Wasn't working for me.
+                latinds = np.intersect1d(
+                    np.where(ds.lat >= aoi.bounds[1]), np.where(ds.lat <= aoi.bounds[3])
+                )
+                ds = ds.isel(dict(lat=latinds))
                 mask = self.get_mask(ds, aoi)
-            else: 
+            else:
                 mask = None
-                
-      
-                
-            
-            if not dry_run:
-                print('beginning download')
-                print('info')
-                print(ds.info())
-                ds.load().to_netcdf(path=join(collection.outdir, 'result.nc'))
-                if not mask is None:
-                    np.save(join(collection.outdir, 'aoi_mask.npy'), mask)
-            else: 
-                print('dry run')
-                print('dataset info')
-                print(' --- ')
+
+            if dry_run:
+                print("dry run")
+                print("dataset info")
+                print(" --- ")
                 print(ds.info())
                 ds = ds.to_dataframe()
-                print('memory usage and info')
+                print("memory usage and info")
                 print(ds.info())
-                print(' --- ')
-                
-            return True
+                print(" --- ")
+            if not dry_run:
+                print("beginning download")
+                print("info")
+                print(ds.info())
+                ds.load().to_netcdf(path=join(collection.outdir, "result.nc"))
+                if mask is not None:
+                    np.save(join(collection.outdir, "aoi_mask.npy"), mask)
+
+        return True
